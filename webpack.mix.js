30e7b77a1f939e7fc842306c1b9142c4cdf48164
Andy M. Newhouse <hi@andymnewhouse.me>
2020-06-24 22:50:03 -0500

    Add video option

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,9 +14,14 @@ let mix = require('laravel-mix');
 mix.js('resources/js/site.js', 'public/js');
 
 mix.postCss('resources/css/site.css', 'public/css', [
+    require('postcss-import'),
     require('tailwindcss'),
+    require('postcss-nested'),
 ]);
 
+if (mix.inProduction()) {
+    mix.version();
+}
 
 /*
  |--------------------------------------------------------------------------
